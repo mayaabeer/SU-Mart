01b22a9744c567e2f4ad2b84f30eef7510550a8b
JoshuaMatthewE <115613378+JoshuaMatthewE@users.noreply.github.com>
2024-04-08 15:13:22 +0700

    Update validateuser.php

--- validateuser.php
+++ validateuser.php
@@ -1,35 +1,50 @@
 <html>
-    <head>
-    </head>
-        <body>
-            <?php
-            $servername = "localhost";
-            $username = "Joshua";
-            $password = "password"; 
-            $database = "shopping"; 
+<head>
+<script language="JavaScript" type="text/JavaScript">
+function updateUser(username) {
+    var ajaxUser = document.getElementById("userinfo");
+    ajaxUser.innerHTML = "Welcome " + username + "&nbsp;&nbsp;&nbsp;<a href=\"logout.php\">Log Out</a>";
+}
+</script>
+</head>
+<body>
+<?php
+include('menu.php');
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+$servername = "localhost";
+$username = "Joshua";
+$password = "password"; 
+$database = "shopping"; 
             
-            $conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
             
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-            $query = "SELECT email_address, password, complete_name FROM customers WHERE email_address like '" . $_POST['emailaddress'] . "' " . "AND password like (PASSWORD('" . $_POST['password'] . "'))";
-            $result = mysqli_query($conn, $query) or die(mysql_error());
-            if (mysqli_num_rows($result) == 1) {
-                while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-                    extract($row);
-                    echo "Welcome " . $complete_name . " to SU Mart. <br>";
-                }
-            }
-            else {
-                ?>
-                Invalid Email and/or Password<br>
-                Not registered?
-                <a href="validatesignup.php">Click here</a> to register.<br><br><br>
-                <a href="signin.php">Click here</a> to login again.<br>
-                <?php
-            }
-            ?>
-        </body>
+$query = "SELECT email_address, password, complete_name, type FROM customers WHERE email_address like '" . $_POST['emailaddress'] . "' " . "AND password like (PASSWORD('" . $_POST['password'] . "'))";
+$result = mysqli_query($conn, $query) or die(mysql_error());
+if (mysqli_num_rows($result) == 1) {
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        extract($row);
+        echo "Welcome " . $complete_name . " to SUMart <br>";
+        $_SESSION['emailaddress'] = $_POST['emailaddress'];
+        $_SESSION['password'] = $_POST['password'];
+        $_SESSION['complete_name'] = $complete_name;
+        $_SESSION['type'] = $type;
+        echo "<SCRIPT LANGUAGE=\"JavaScript\">updateUser('$complete_name');</SCRIPT>";
+    }
+}
+else {
+?>
+Invalid Email address and/or Password<br>
+Not registered? <a href="validatesignup.php">Click here</a> to register.<br><br><br>
+Want to try again?<br>
+<a href="signin.php">Click here</a> to try login again.<br>
+<?php
+}
+?>
+</body>
 </html>
