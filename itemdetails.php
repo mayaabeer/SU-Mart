0cc52e24a8ca465964dd44ac59c2f82303194b5e
JoshuaMatthewE <115613378+JoshuaMatthewE@users.noreply.github.com>
2024-04-20 21:00:39 +0700

    Update itemdetails.php

--- itemdetails.php
+++ itemdetails.php
@@ -1,7 +1,7 @@
 <?php
 
 if (session_status() == PHP_SESSION_NONE) {
-session_start();
+    session_start();
 }
 
 include('menu.php');
@@ -22,15 +22,15 @@ if (isset($_SESSION['user_id'])) {
     $session_id = $_SESSION['user_id'];
 }
 
-$code=$_REQUEST['itemcode'];
+$code = $_REQUEST['itemcode'];
 $query = "SELECT item_code, item_name, description, imagename, price, user_id FROM products WHERE item_code = '$code'";
 $results = mysqli_query($conn, $query) or die(mysql_error());
 $row = mysqli_fetch_array($results, MYSQLI_ASSOC);
 extract($row);
 
-$itemname=urlencode($item_name);
-$itemprice=$price;
-$itemdescription=$description;
+$itemname = urlencode($item_name);
+$itemprice = $price;
+$itemdescription = $description;
 $pfquery = "SELECT feature1, feature2, feature3, feature4, feature5, feature6 FROM productfeatures WHERE item_code = '$code'";                                        
 $pfresults = mysqli_query($conn, $pfquery) or die(mysql_error());
 $pfrow = mysqli_fetch_array($pfresults, MYSQLI_ASSOC);
@@ -58,10 +58,8 @@ extract($pfrow);
     <div class="item-info">
         <h2><?php echo $item_name; ?></h2>
 
-        //This is sample code, replace it with something else
-        
         <h2><?php if ($session_id ==! null && $session_id == $user_id) {
-            echo "You own this product";
+            echo '<a href="#" onclick="editProduct()">Edit product</a>';
         }
         ?></h2>
 
@@ -94,6 +92,35 @@ extract($pfrow);
     </div>
 </div>
 
+<div id="editForm" style="display: none;">
+    <h2>Edit Product: <?php echo $item_name; ?></h2>
+    <form method="POST" action="update_product.php">
+        <input type="hidden" name="item_code" value="<?php echo $item_code; ?>">
+        <label for="item_name">Item Name:</label>
+        <input type="text" id="item_name" name="item_name" value="<?php echo $item_name; ?>"><br>
+        <label for="description">Description:</label>
+        <textarea id="description" name="description"><?php echo $description; ?></textarea><br>
+        <label for="price">Price:</label>
+        <input type="text" id="price" name="price" value="<?php echo $price; ?>"><br>
+        <label for="feature1">Feature 1:</label>
+        <input type="text" id="feature1" name="feature1" value="<?php echo $feature1; ?>"><br>
+        <label for="feature2">Feature 2:</label>
+        <input type="text" id="feature2" name="feature2" value="<?php echo $feature2; ?>"><br>
+        <label for="feature3">Feature 3:</label>
+        <input type="text" id="feature3" name="feature3" value="<?php echo $feature3; ?>"><br>
+        <label for="feature4">Feature 4:</label>
+        <input type="text" id="feature4" name="feature4" value="<?php echo $feature4; ?>"><br>
+        <label for="feature5">Feature 5:</label>
+        <input type="text" id="feature5" name="feature5" value="<?php echo $feature5; ?>"><br>
+        <label for="feature6">Feature 6:</label>
+        <input type="text" id="feature6" name="feature6" value="<?php echo $feature6; ?>"><br>
+
+        <input type="submit" name="update_product" value="Update Product">
+    </form>
+</div>
+
+
+
 <script>
     function dec() {
         let num = document.querySelector('#quantityInput');
@@ -117,6 +144,11 @@ extract($pfrow);
 
     function updateQuantity() {
         updateHiddenInputValue();
+    }
+
+    function editProduct() {
+        document.getElementById("editForm").style.display = "block";
+        document.querySelector(".item-info").style.display = "none";
     }
 </script>
 </body>
